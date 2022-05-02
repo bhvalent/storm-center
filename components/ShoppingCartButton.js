362c783255fd6a683462f1d58a5c687a3f6b0dbf
bhvalent <benhudvalentine@gmail.com>
2022-05-02 14:46:00 -0500

    Added shopping cart total badge

--- components/ShoppingCartButton.js
+++ components/ShoppingCartButton.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Text, Pressable } from 'react-native'
 import FontAwesomeIcon from '@expo/vector-icons/FontAwesome';
 import { getColors } from '../constants/theme';
+import { useShopData } from '../data/ShopData';
+
+const colors = getColors();
 
 export default function ShoppingCartButton({ style, onPress, ...rest }) {
-  const colors = getColors();
+  const { state } = useShopData();
+  const shoppingCartCount = state.shoppingCart.length;
+
+  let badge = null;
+  if (shoppingCartCount > 0) {
+    badge = (
+      <View style={styles.badge}>
+        <Text style={styles.badgeText}>{shoppingCartCount}</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={[styles.button, style]} {...rest}>
-      <FontAwesomeIcon color={colors.white} size={28} name='shopping-cart' onPress={onPress} />
+      {badge}
+      <FontAwesomeIcon color={colors.white} size={28} name='shopping-cart'  onPress={onPress}/>
     </View>
   )
 }
@@ -16,6 +30,23 @@ export default function ShoppingCartButton({ style, onPress, ...rest }) {
 const styles = StyleSheet.create({
   button: {
     marginVertical: 10,
-    marginHorizontal: 20
+    marginHorizontal: 20,
+    flex: 1
+  },
+  badgeText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 12,
+    color: 'white',
+  },
+  badge: {
+    backgroundColor: colors.lightBlue,
+    borderRadius: 15,
+    position: 'absolute',
+    paddingVertical: 1,
+    paddingHorizontal: 5,
+    zIndex: 1,
+    right: -10,
+    top: -10
   }
 })
