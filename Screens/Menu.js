bd8e6add870647d80c0e2ed28bfc299aae5bab47
bhvalent <benhudvalentine@gmail.com>
2022-05-04 13:48:05 -0500

    Added cards to the menu screen

--- Screens/Menu.js
+++ Screens/Menu.js
@@ -1,20 +1,75 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import PressableCard from '../components/ui/PressableCard';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import RegularText from '../components/ui/RegularText';
+import { getColors } from '../constants/theme';
 
-export default function Menu({ style, ...rest }) {
+const colors = getColors();
+const windowWidth = Dimensions.get('window').width;
+
+export default function Menu() {
   return (
-    <View style={[styles.container, style]} {...rest}>
-      <RegularText>Behold, the Menu!</RegularText>
+    <View style={styles.screen}>
+      <RegularText style={styles.menuHeader}>Menu</RegularText>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.row}>
+          <PressableCard style={styles.card}>
+            <View style={styles.cardContents}>
+              <FontAwesome5 name="truck" size={36} color={colors.veryLightGrey} />
+              <RegularText>Orders</RegularText>
+            </View>
+          </PressableCard>
+          <PressableCard style={styles.card}>
+            <View style={styles.cardContents}>
+              <FontAwesome5 name="credit-card" size={36} color={colors.veryLightGrey} />
+              <RegularText>Payment</RegularText>
+            </View>
+          </PressableCard>
+        </View>
+        <View style={styles.row}>
+          <PressableCard style={styles.card}>
+            <View style={styles.cardContents}>
+              <FontAwesome5 name="tools" size={36} color={colors.veryLightGrey} />
+              <RegularText>Tools</RegularText>
+            </View>
+          </PressableCard>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
+  },
+  buttonsContainer: {
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  menuHeader: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginHorizontal: 20,
+    borderBottomColor: colors.mediumGrey,
+    borderBottomWidth: 2
+  },
+  row: {
+    flexDirection: 'row',
+    width: windowWidth * 0.9,
+    justifyContent: 'space-around',
+    marginVertical: 10
+  },
+  card: {
+    backgroundColor: colors.darkGrey,
+    marginHorizontal: 10,
+    width: '40%'
+  },
+  cardContents: {
+    alignItems: 'center',
+    paddingTop: 15,
+    paddingBottom: 8
   }
 })
--- Screens/shop/ProductsOverview.js
+++ Screens/shop/ProductsOverview.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, FlatList, StyleSheet, Image } from 'react-native';
+import { View, FlatList, StyleSheet, Image, Dimensions } from 'react-native';
 
 import PRODUCTS from '../../data/dummy-products'
 import PressableCard from '../../components/ui/PressableCard'
@@ -7,6 +7,7 @@ import { getColors } from '../../constants/theme';
 import RegularText from '../../components/ui/RegularText';
 
 const colors = getColors();
+const windowWidth = Dimensions.get('window').width;
 
 export default function ProductsOverview({ style, navigation, ...rest }) {
   const products = PRODUCTS;
@@ -30,13 +31,18 @@ export default function ProductsOverview({ style, navigation, ...rest }) {
   }
 
   return (
-    <FlatList data={products} keyExtractor={item => item.id} renderItem={renderItem} />
+    <FlatList contentContainerStyle={styles.list} data={products} keyExtractor={item => item.id} renderItem={renderItem} />
   );
 }
 
 const styles = StyleSheet.create({
+  list: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   card: {
-    margin: 10,
+    width: windowWidth * 0.9,
+    marginVertical: 10,
     backgroundColor: colors.darkGrey
   },
   cardContents: {
